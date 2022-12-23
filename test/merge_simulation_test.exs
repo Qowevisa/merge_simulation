b0555f1e1e29186283a747f18e4cce5f330329ea
Qowevisa <qowevisa@gmail.com>
2022-12-24 00:02:23 +0200

    Add tests for little endian merge

--- test/merge_simulation_test.exs
+++ test/merge_simulation_test.exs
@@ -2,6 +2,8 @@ defmodule MergeSimulationTest do
   use ExUnit.Case
   doctest MergeSimulation
 
+  ## Testing high endian merge
+
   test "center merge list" do
     assert MergeSimulation.merge_list([2, 1, 1, 0]) == [2, 2, 0]
   end
@@ -18,4 +20,21 @@ defmodule MergeSimulationTest do
     assert MergeSimulation.merge_list([2, 1, 0]) == [2, 1, 0]
   end
 
+  ## Testing little endian merge
+
+  test "center little endian merge" do
+    assert MergeSimulation.merge_list_little_endian([4, 3, 3, 2, 1]) == [4, 4, 2, 1]
+  end
+
+  test "left little endian merge" do
+    assert MergeSimulation.merge_list_little_endian([3, 3, 2, 1]) == [4, 2, 1]
+  end
+
+  test "right little endian merge" do
+    assert MergeSimulation.merge_list_little_endian([2, 1, 0, 0]) == [2, 1, 1]
+  end
+
+  test "no little endian merge" do
+    assert MergeSimulation.merge_list_little_endian([4, 3, 2, 1, 0]) == [4, 3, 2, 1, 0]
+  end
 end
