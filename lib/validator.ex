84b0afccf0d04e451ee564a9f826ace6dcb4604f
Qowevisa <qowevisa@gmail.com>
2022-12-29 09:55:51 +0200

    Refactor Validator to be more elegant in some sort

--- lib/validator.ex
+++ lib/validator.ex
@@ -1,50 +1,36 @@
 defmodule Validator do
-  defmacro do_validate(value, :num) do
-    quote do
-      value = unquote(value)
-
-      {sign, num} =
-        case value do
-          {sign, _, [num]} -> {sign, num}
-          num -> {:+, num}
-        end
-
-      if sign == :- do
-        {:error,
-         "Inappropriate first argument! Expected value greater than zero but got #{sign}#{inspect(num)}"}
-      else
-        {:ok, value}
-      end
+  @spec __do_validate__(any, :endian | :inspect | :num) ::
+          {:error, <<_::64, _::_*8>>} | {:ok, any}
+  def __do_validate__(value, :num) do
+    if value < 0 do
+      {:error,
+       "Inappropriate first argument! Expected value greater than zero but got #{inspect(value)}"}
+    else
+      {:ok, value}
     end
   end
 
-  defmacro do_validate(value, :endian) do
-    quote do
-      value = unquote(value)
-
-      if not (value == :high or value == :low) do
-        {:error,
-         "Inappropriate second argument! Expected :high or :low but got #{inspect(value)}"}
-      else
-        {:ok, value}
-      end
+  def __do_validate__(value, :endian) do
+    if not (value == :high or value == :low) do
+      {:error, "Inappropriate second argument! Expected :high or :low but got #{inspect(value)}"}
+    else
+      {:ok, value}
     end
   end
 
-  defmacro do_validate(value, :inspect) do
-    quote do
-      value = unquote(value)
-
-      if not (value == :yes or value == :no) do
-        {:error, "Inappropriate third argument! Expected :yes or :no but got #{inspect(value)}"}
-      end
+  def __do_validate__(value, :inspect) do
+    if not (value == :yes or value == :no) do
+      {:error, "Inappropriate third argument! Expected :yes or :no but got #{inspect(value)}"}
+    else
+      {:ok, value}
     end
   end
 
-  defmacro validate(num, endian) do
-    case do_validate(num, :num) do
+  @spec __define__(integer(), atom()) :: {:error, <<_::64, _::_*8>>} | {:ok, :empty}
+  def __define__(num, endian) do
+    case __do_validate__(num, :num) do
       {:ok, _} ->
-        case do_validate(endian, :endian) do
+        case __do_validate__(endian, :endian) do
           {:ok, _} -> {:ok, :empty}
           {:error, msg} -> {:error, msg}
         end
@@ -54,9 +40,35 @@ defmodule Validator do
     end
   end
 
+  @spec __define__(integer(), atom(), atom()) :: {:error, <<_::64, _::_*8>>} | {:ok, :empty}
+  def __define__(num, endian, inspect) do
+    case __do_validate__(num, :num) do
+      {:ok, _} ->
+        case __do_validate__(endian, :endian) do
+          {:ok, _} ->
+            case __do_validate__(inspect, :inspect) do
+              {:ok, _} -> {:ok, :empty}
+              {:error, msg} -> {:error, msg}
+            end
+
+          {:error, msg} ->
+            {:error, msg}
+        end
+
+      {:error, msg} ->
+        {:error, msg}
+    end
+  end
+
+  defmacro validate(num, endian) do
+    quote do
+      Validator.__define__(unquote(num), unquote(endian))
+    end
+  end
+
   defmacro validate(num, endian, inspect) do
-    do_validate(num, :num)
-    do_validate(endian, :endian)
-    do_validate(inspect, :inspect)
+    quote do
+      Validator.__define__(unquote(num), unquote(endian), unquote(inspect))
+    end
   end
 end
